d4d3f36da8873d93d61c781bdda0f97523959d4b
paladinescamila <camilapaladines27@gmail.com>
2021-04-01 14:02:39 -0500

    Add time measures

--- parallel.c
+++ parallel.c
@@ -17,33 +17,23 @@ void Build_Array(int array[], int n){
 }
 
 
-double Original_BS(int array[], int n, int max, int min) {
+void Original_BS(int array[], int n, int max, int min) {
 
     int i, j = 0;
     int cpos[max+1], cneg[-(min-1)];
-    double t_start, t_wall_clock, time = 0;
 
-    t_start = omp_get_wtime();
     for (i = 0; i <= max; i++)
         cpos[i] = 0;
     for (i = 0; i <= -(min-1); i++)
         cneg[i] = 0;
-    t_wall_clock = omp_get_wtime() - t_start;
-    time += t_wall_clock;
-    printf("Part 1: %f\n", t_wall_clock);
 
-    t_start = omp_get_wtime();
     for (i = 0; i < n; i++) {
         if (array[i] >= 0)
             (cpos[array[i]])++;
         else
             (cneg[-array[i]])++;
     }
-    t_wall_clock = omp_get_wtime() - t_start;
-    time += t_wall_clock;
-    printf("Part 2: %f\n", t_wall_clock);
 
-    t_start = omp_get_wtime();
     for (i = -min; i > 0; i--)
         for (; cneg[i] > 0; (cneg[i])--)
             array[j++] = -i;
@@ -51,22 +41,15 @@ double Original_BS(int array[], int n, int max, int min) {
     for (i = 0; i <= max; i++)
         for (; cpos[i] > 0; (cpos[i])--)
             array[j++] = i;
-    
-    t_wall_clock = omp_get_wtime() - t_start;
-    time += t_wall_clock;
-    printf("Part 3: %f\n", t_wall_clock);
 
-    return time;
 }
 
 
-double Parallel_BS(int array[], int n, int max, int min) {
+void Parallel_BS(int array[], int n, int max, int min) {
 
     int i, j = 0;
     int cpos[max+1], cneg[-(min-1)];
-    double t_start, t_wall_clock, time = 0;
 
-    t_start = omp_get_wtime();
     #pragma omp parallel for
     for (i = 0; i <= max; i++)
         cpos[i] = 0;
@@ -74,11 +57,7 @@ double Parallel_BS(int array[], int n, int max, int min) {
     #pragma omp parallel for
     for (i = 0; i <= -(min-1); i++)
         cneg[i] = 0;
-    t_wall_clock = omp_get_wtime() - t_start;
-    time += t_wall_clock;
-    printf("Part 1: %f\n", t_wall_clock);
 
-    t_start = omp_get_wtime();
     #pragma omp parallel for
     for (i = 0; i < n; i++) {
         if (array[i] >= 0)
@@ -86,11 +65,7 @@ double Parallel_BS(int array[], int n, int max, int min) {
         else
             (cneg[-array[i]])++;
     }
-    t_wall_clock = omp_get_wtime() - t_start;
-    time += t_wall_clock;
-    printf("Part 2: %f\n", t_wall_clock);
 
-    t_start = omp_get_wtime();
     #pragma omp parallel for
     for (i = -min; i > 0; i--)
         for (; cneg[i] > 0; (cneg[i])--)
@@ -100,12 +75,7 @@ double Parallel_BS(int array[], int n, int max, int min) {
     for (i = 0; i <= max; i++)
         for (; cpos[i] > 0; (cpos[i])--)
             array[j++] = i;
-    
-    t_wall_clock = omp_get_wtime() - t_start;
-    time += t_wall_clock;
-    printf("Part 3: %f\n", t_wall_clock);
 
-    return time;
 }
 
 
@@ -125,18 +95,16 @@ int main() {
     if (min >= 0)
         min = 0;
 
-    printf("Original\n");
-    s_original = omp_get_wtime();
-    double time_o = Original_BS(array, n, max, min);
-    t_original = omp_get_wtime() - s_original;
+    // s_original = omp_get_wtime();
+    // Original_BS(array, n, max, min);
+    // t_original = omp_get_wtime() - s_original;
 
-    printf("\nParallel\n");
     s_parallel = omp_get_wtime();
-    double time_p = Parallel_BS(array, n, max, min);
+    Parallel_BS(array, n, max, min);
     t_parallel = omp_get_wtime() - s_parallel;
+    printf("Time: %f\n", t_parallel);
     
-    printf("\nSpeedup: %f/%f = %f\n", time_o, time_p, time_o / time_p);
-    printf("Speedup: %f/%f = %f\n", t_original, t_parallel, t_original / t_parallel);
+    // printf("Speedup: %f/%f = %f\n", t_original, t_parallel, t_original / t_parallel);
 
     return 0;
 }
