c7f43af6935594f39985fdcb1789ff262772e3f7
paladinescamila <camilapaladines27@gmail.com>
2021-04-01 15:45:24 -0500

    Tests

--- parallel.c
+++ parallel.c
@@ -17,34 +17,6 @@ void Build_Array(int array[], int n){
 }
 
 
-void Original_BS(int array[], int n, int max, int min) {
-
-    int i, j = 0;
-    int cpos[max+1], cneg[-(min-1)];
-
-    for (i = 0; i <= max; i++)
-        cpos[i] = 0;
-    for (i = 0; i <= -(min-1); i++)
-        cneg[i] = 0;
-
-    for (i = 0; i < n; i++) {
-        if (array[i] >= 0)
-            (cpos[array[i]])++;
-        else
-            (cneg[-array[i]])++;
-    }
-
-    for (i = -min; i > 0; i--)
-        for (; cneg[i] > 0; (cneg[i])--)
-            array[j++] = -i;
-
-    for (i = 0; i <= max; i++)
-        for (; cpos[i] > 0; (cpos[i])--)
-            array[j++] = i;
-
-}
-
-
 void Parallel_BS(int array[], int n, int max, int min) {
 
     int i, j = 0;
@@ -95,16 +67,10 @@ int main() {
     if (min >= 0)
         min = 0;
 
-    // s_original = omp_get_wtime();
-    // Original_BS(array, n, max, min);
-    // t_original = omp_get_wtime() - s_original;
-
     s_parallel = omp_get_wtime();
     Parallel_BS(array, n, max, min);
     t_parallel = omp_get_wtime() - s_parallel;
     printf("Time: %f\n", t_parallel);
-    
-    // printf("Speedup: %f/%f = %f\n", t_original, t_parallel, t_original / t_parallel);
 
     return 0;
 }
